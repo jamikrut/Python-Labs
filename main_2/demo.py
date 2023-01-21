d0f527767645b9299f13edea6a50d1dd2b870db3
jamikrut <jamikrut@student.wszib.edu.pl>
2023-01-21 14:42:51 +0100

    commit changes for loop examples

--- main_2/demo.py
+++ main_2/demo.py
@@ -4,3 +4,13 @@ i = 0
 while i < 10:
     print(i, end=" ")
     i += 1
+
+print("\n")
+
+for i in range(0,10): # i < 10
+    print(i, end=" ")
+
+print("\n")
+
+for i in range(10): # to same co range (0, 10, 1)
+    print(i, end=" ")
