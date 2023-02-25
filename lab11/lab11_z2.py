f4d10d8a3a3cedf9af2486461d63233f3bb38c2e
jamikrut <jamikrut@student.wszib.edu.pl>
2023-02-25 13:48:47 +0100

    Added lab12

---
+++ lab11/lab11_z2.py
@@ -0,0 +1,4 @@
+# Napisz skrypt pobierający od użytkownika serię liczb całkowitych,
+# a następnie wyświetl je w kolejności malejącej pozbywając się wcześniej duplikatów.
+
+lnght = int(input("Podaj dlugość serii licz: "))
